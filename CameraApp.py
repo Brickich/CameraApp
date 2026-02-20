77987b41c9e17f989079ebec92ae7caa91885cfe
Kirill <kstarcev73@gmail.com>
2026-02-20 10:59:12 +0700

    changed minimal frames quantity for trigger

--- CameraApp.py
+++ CameraApp.py
@@ -204,7 +204,7 @@ class CameraSettingsFrame(QWidget):
                         dataType = float
                 except:
                     if setting == "FramesQuantity":
-                        slider_range = {"max": 1000, "min": 10, "inc": 1}
+                        slider_range = {"max": 1000, "min": 5, "inc": 1}
                         value = self.camera.FramesQuantity
                         dataType = int
 
@@ -1477,7 +1477,7 @@ class FrameViewer(QWidget):
             cam_attr["imageLabel"].setPixmap(scaled_pixmap)
             cam_attr["infoLabel"].setText(f"Frame: {new_index}\nTime: {timestamps[new_index]} μs")
 
-    def _toggle_video(self, checked):
+    def _toggle_video(self, checked): 
         self.is_playing = checked
 
         if checked:
